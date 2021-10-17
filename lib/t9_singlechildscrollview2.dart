70eec8fcafa0cb0c1b17a72292a53306aef696e2
zidi <zidi@outlook.com>
2021-10-17 17:46:44 +0800

    checkpoint

---
+++ lib/t9_singlechildscrollview2.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    title: 'Generalzzd SingleChildScrollView Demo',
+    home: ZSingleChildScrollView()));
+
+class ZSingleChildScrollView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ZSingleChildScrollViewState();
+}
+
+class ZSingleChildScrollViewState extends State<ZSingleChildScrollView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+                "The widget that is to grow to fit the remaining space so provided is wrapped in an Expanded widget. This technique is quite expensive, as it more or less requires that the contents of the viewport be laid out twice (once to find their intrinsic dimensions, and once to actually lay them out). The number of widgets within the column should therefore be kept small. Alternatively, subsets of the children that have known dimensions can be wrapped in a SizedBox that has tight vertical constraints, so that the intrinsic sizing algorithm can short-circuit the computation when it reaches those parts of the subtree."),
+            Container(
+              width: 500,
+              height: 600,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
